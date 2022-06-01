870f2b6451f16a7f35cc029885465b37f1e0ff0b
@antipin.a <antipin.a@robotkarl.com>
2022-06-02 02:06:39 +0300

    migra

--- migrations/m220601_230430_license.php
+++ migrations/m220601_230430_license.php
@@ -7,9 +7,50 @@ use yii\db\Migration;
  */
 class m220601_230430_license extends Migration
 {
+
+
     /**
      * {@inheritdoc}
      */
+
+    public function up()
+    {
+//Добавляем все необходимые поля
+        $this->createTable('license', [
+            'id' => $this->primaryKey()->notNull(),
+            'name' => $this->char(255)->defaultValue(null),
+            'serial' => $this->char(255)->defaultValue(null),
+            'public_key' => $this->char(255)->defaultValue(null),
+            'private_key' => $this->char(255)->defaultValue(null),
+            'status' => $this->char(255)->defaultValue(null),
+            'mail' => $this->char(255)->defaultValue(null),
+            'commit' => $this->char(255)->defaultValue(null),
+            'id_member' => $this->char(255)->defaultValue(null),
+            'type' => $this->char(255)->defaultValue(null),
+            'inn' => $this->char(255)->defaultValue(null),
+            'payment' => $this->char(255)->defaultValue(null),
+            'valid_date' => $this->char(255)->defaultValue(null),
+            'date_start' => $this->char(255)->defaultValue(null),
+            'temp1' => $this->char(255)->defaultValue(null),
+            'temp2' => $this->char(255)->defaultValue(null),
+            'temp3' => $this->char(255)->defaultValue(null),
+            'temp4' => $this->char(255)->defaultValue(null),
+            'temp5' => $this->char(255)->defaultValue(null),
+            'temp6' => $this->char(255)->defaultValue(null),
+            'lic_type' => $this->integer(),
+            'temp7' => $this->integer(),
+            'temp8' => $this->integer(),
+            'temp9' => $this->integer(),
+           
+        ]);
+
+    }
+  public function down()
+      {
+          $this->dropTable('scor');
+         
+      }
+
     public function safeUp()
     {
 
