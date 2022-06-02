08d2a5d7fd059d5054b70bd29fce3ad6472f5bbb
@antipin.a <antipin.a@robotkarl.com>
2022-06-03 00:54:34 +0300

    sss

--- config/web.php
+++ config/web.php
@@ -7,6 +7,7 @@ $config = [
     'id' => 'basic',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
+    'defaultRoute' => 'license/index',
     'aliases' => [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
@@ -64,7 +65,7 @@ $config = [
         
     ],
     'params' => $params,
-    'defaultRoute' => 'license/index',
+    
 ];
 
 if (YII_ENV_DEV) {
