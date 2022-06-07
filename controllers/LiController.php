17fe138a42fabdb4f27a19ec18d8b4d5ea6fa38a
@antipin.a <antipin.a@robotkarl.com>
2022-06-07 18:04:07 +0300

    sadsa

--- controllers/LiController.php
+++ controllers/LiController.php
@@ -35,15 +35,24 @@ class LiController extends ActiveController
 
     header('Content-type: application/json');
 
-        echo JSON::encode($customer);   
+        //echo JSON::encode($customer);   
         $id = Yii::$app->request->get('id');
         $name = Yii::$app->request->get('name');
-        $user = Yii::$app->request->get('namusere');
+        $user = Yii::$app->request->get('user');
         $userSerial = Yii::$app->request->get('userSerial');
         $userPKey = Yii::$app->request->get('userPKey');
-        if(isset($name,$user,$userSerial,$userPKey))
+        if(isset($name,$user,$userSerial,$userPKey,$id))
         {
-            echo '1111111111111111111';
+            //echo '1111111111111111111';
+            $model =License::find()-where(['public_key'=>$id])->one();
+            $model->private_key =$userSerial;
+            $model->name =$name;
+            $model->mail =$user ;
+            $model->save();
+            //Licence::Update(['status' => 1], ['like', 'email', '@dummy.com']);
+            
+            $customer = License::findOne(['public_key' =>Yii::$app->request->get('id') ]);
+            echo JSON::encode($customer);
         }
 
 
